34d70c29cef573a573f02ce9ef96d44966919ac7
Tim von Oldenburg <tim@tvooo.de>
2016-06-05 17:15:59 +0100

    Add validation to comment form; not styles

--- app/client/templates/elements/comment.js
+++ app/client/templates/elements/comment.js
@@ -16,29 +16,38 @@ Template.comment.events({
     }
 })
 
-Template.createComment.events({
-    'submit .comment-form': (event) => {
-        event.preventDefault();
+Template.createComment.rendered = function() {
+    $("#comment-form").validate({
+        submitHandler: function(form, event) {
+            let comment = {
+              title: event.target.title.value,
+              text: event.target.text.value,
+              postId: event.target.postId.value,
+              createdAt: new Date(),
+              published: true
+            };
 
-        let comment = {
-          title: event.target.title.value,
-          text: event.target.text.value,
-          postId: event.target.postId.value,
-          createdAt: new Date(),
-          published: true
-        };
+            if(Meteor.userId()) {
+              comment.createdBy = Meteor.userId();
+            }
 
-        if(Meteor.userId()) {
-          comment.createdBy = Meteor.userId();
+            Meteor.call('createComment', comment, (error, response) => {
+              if(error) {
+                Bert.alert( error.reason, "warning" );
+              } else {
+                Bert.alert( 'Danke für deinen Kommentar!', 'info', 'growl-top-right' );
+                Session.set('showCreateComment', false);
+              }
+            });
         }
+    });
+}
 
-        Meteor.call('createComment', comment, (error, response) => {
-          if(error) {
-            Bert.alert( error.reason, "warning" );
-          } else {
-            Bert.alert( 'Danke für deinen Kommentar!', 'info', 'growl-top-right' );
-            Session.set('showCreateComment', false);
-          }
-        });
+Template.createComment.events({
+    'submit #comment-form': (event) => {
+        event.preventDefault();
+    },
+    'click .cancel': () => {
+        Session.set('showCreateComment', false);
     }
 })
