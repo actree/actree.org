60cc2705618b736099d42c9bd96df8bfb89be828
Tim von Oldenburg <tim@tvooo.de>
2016-06-15 20:08:19 +0100

    Add auth support for import

--- import.sh
+++ import.sh
@@ -15,15 +15,15 @@ collections=(
     nicolaslopezj_roles_keys
 )
 
-if [ $1 && $2 ]; then
+if [ $1 ] && [ $2 ]; then
     echo "Importing collection $1"
-    mongoimport -d $MONGO_METEOR_DB -c $1 --file=$2
+    mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB -c $1 --file=$2
 else
     echo "Importing all collections"
 
     for i in "${collections[@]}"
     do
         echo "Importing " $i
-        mongoexport -d $MONGO_METEOR_DB -c $i --file=export/$i.json
+        mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB -c $i --file=export/$i.json
     done
 fi
