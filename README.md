81f943bb937fe026ff14aa101ffe78b2aa2a1637
Tim von Oldenburg <tim@tvooo.de>
2016-06-15 21:17:02 +0100

    Started to add some documentation

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # actree.org
-The actree blog, later the whole website
+
+actree.org - built with Meteor, some shell scripts, and glue
+
+## Import and Export data
+
+Use `export.sh` and `import.sh`
+
+## Run on Uberspace
+
+https://wiki.uberspace.de/system:daemontools
+
+### Start
+
+1. Start MongoDB: `svc -u ~/service/mongodb`
+2. Start Meteor: `svc -u ~/service/meteor`
+
+### Stop
+
+1. Stop Meteor: `svc -d ~/service/meteor`
+2. Stop MongoDB: svc -d ~/service/meteor
--- import.sh
+++ import.sh
@@ -17,13 +17,13 @@ collections=(
 
 if [ $1 ] && [ $2 ]; then
     echo "Importing collection $1"
-    mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB -c $1 --file=$2
+    mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB --authenticationDatabase admin -c $1 --file=$2
 else
     echo "Importing all collections"
 
     for i in "${collections[@]}"
     do
         echo "Importing " $i
-        mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB -c $i --file=export/$i.json
+        mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB --authenticationDatabase admin -c $i --file=export/$i.json
     done
 fi
