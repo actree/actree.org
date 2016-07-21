78706d2560f2b32c8bce659b880de63b4bdf029e
Tim von Oldenburg <tim@tvooo.de>
2016-07-21 23:44:18 +0100

    Fix import script

--- import.sh
+++ import.sh
@@ -1,29 +1,25 @@
 #!/bin/bash
 
 collections=(
-    posts
-    comments
-
-    entry
-
-    orion_config
-    dictionary
-    tabular_records
-
+    app_updates
     users
-    roles
-    nicolaslopezj_roles_keys
+
+    entries
+    pages
+    tags
+    posts
+    postcategories
 )
 
 if [ $1 ] && [ $2 ]; then
     echo "Importing collection $1"
-    mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB --authenticationDatabase admin -c $1 --file=$2
+    mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_DB --authenticationDatabase admin -c $1 --file=$2
 else
     echo "Importing all collections"
 
     for i in "${collections[@]}"
     do
         echo "Importing " $i
-        mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_METEOR_DB --authenticationDatabase admin -c $i --file=export/$i.json
+        mongoimport --port $MONGO_PORT -u $MONGO_USER -p $MONGO_PASSWORD -d $MONGO_DB --authenticationDatabase admin -c $i --file=export/$i.json
     done
 fi
