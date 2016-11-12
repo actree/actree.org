a11fa4a573dc51de822232da14e25685927ad0fa
Tim von Oldenburg <tim@tvooo.de>
2016-11-12 15:25:08 +0000

    Fix issue with avatar images

--- keystone/models/User.js
+++ keystone/models/User.js
@@ -9,7 +9,7 @@ var User = new keystone.List('User');
 
 User.add({
 	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true, displatGravatar: true },
+	email: { type: Types.Email, initial: true, required: true, index: true },
 	password: { type: Types.Password, initial: true, required: true },
     bio: { type: String },
     avatar: { type: Types.CloudinaryImage },
