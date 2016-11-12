ef4f19321c45f64e33226d9ef34186c0a62d5e5c
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2016-11-12 20:00:32 +0000

    Add cssnano and autoprefixer

--- keystone/gulpfile.js
+++ keystone/gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell')
+var rename = require("gulp-rename");
 
 var sass = require('gulp-sass');
-
+var cssnano = require("gulp-cssnano");
+var autoprefixer = require("gulp-autoprefixer");
 
 var paths = {
 	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
@@ -24,6 +26,19 @@ gulp.task('watch:sass', function () {
 gulp.task('sass', function(){
 	gulp.src(paths.style.all)
 		.pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ["last 3 versions", "IE >= 10"],
+            cascade: false
+        }))
+        .pipe(gulp.dest(paths.style.output))
+        .pipe(rename(function(path) {
+            path.extname = ".min.css";
+        }))
+        .pipe(cssnano({
+            debug: true,
+            zindex: false,
+            autoprefixer: false
+        }))
 		.pipe(gulp.dest(paths.style.output));
 });
 
