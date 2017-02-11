79d0283df6277052e1ef92260b729cef47892d71
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-02-11 17:10:51 +0000

    Optimized images

--- keystone/gulpfile.js
+++ keystone/gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var shell = require('gulp-shell')
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 
 var sass = require('gulp-sass');
-var cssnano = require("gulp-cssnano");
-var autoprefixer = require("gulp-autoprefixer");
+var cssnano = require('gulp-cssnano');
+var autoprefixer = require('gulp-autoprefixer');
+var imageop = require('gulp-image-optimization');
 
 var paths = {
 	'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
@@ -23,16 +24,16 @@ gulp.task('watch:sass', function () {
 	gulp.watch(paths.style.all, ['sass']);
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
 	gulp.src(paths.style.all)
 		.pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
-            browsers: ["last 3 versions", "IE >= 10"],
+            browsers: ['last 4 versions', 'IE >= 9'],
             cascade: false
         }))
         .pipe(gulp.dest(paths.style.output))
         .pipe(rename(function(path) {
-            path.extname = ".min.css";
+            path.extname = '.min.css';
         }))
         .pipe(cssnano({
             debug: true,
@@ -42,12 +43,19 @@ gulp.task('sass', function(){
 		.pipe(gulp.dest(paths.style.output));
 });
 
+gulp.task('images', function(cb) {
+  gulp.src(['srcimages/**/*.png','srcimages/**/*.jpg','srcimages/**/*.gif','srcimages/**/*.jpeg']).pipe(imageop({
+      optimizationLevel: 5,
+      progressive: true,
+      interlaced: true
+  })).pipe(gulp.dest('public/images/entries')).on('end', cb).on('error', cb);
+});
 
 gulp.task('runKeystone', shell.task('node keystone.js'));
 gulp.task('watch', [
-
   'watch:sass',
-
 ]);
+
+gulp.task('build', ['images', 'sass']);
 
 gulp.task('default', ['watch', 'runKeystone']);
