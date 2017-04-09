e5264ff3e674579b0fccbc06742114958530f476
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-04-09 14:59:07 +0100

    Increase entry items per page to 100, try flexbox for main body

--- models/Entry.js
+++ models/Entry.js
@@ -1,32 +1,68 @@
-var keystone = require('keystone');
-var Types = keystone.Field.Types;
+const keystone = require('keystone');
+
+const Types = keystone.Field.Types;
 
 /**
  * Post Model
  * ==========
  */
 
-var Entry = new keystone.List('Entry', {
-    map: { name: 'title' },
-    autokey: { path: 'slug', from: 'title', unique: true },
+const Entry = new keystone.List('Entry', {
+  map: { name: 'title' },
+  autokey: { path: 'slug', from: 'title', unique: true },
 });
 
 Entry.add({
-    title: { type: String, required: true },
-    url: { type: Types.Url },
-    state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
-    author: { type: Types.Relationship, ref: 'User', index: true },
-    publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
-    image: { type: Types.CloudinaryImage },
-    oldImage: { type: Types.Url },
-    description: { type: Types.Markdown, height: 600 },
-    descriptionEnglish: { type: Types.Markdown, height: 200, hidden: true },
-    tags: { type: Types.Relationship, ref: 'Tag', many: true }
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: Types.Url,
+  },
+  state: {
+    type: Types.Select,
+    options: 'draft, published, archived',
+    default: 'draft',
+    index: true,
+  },
+  author: {
+    type: Types.Relationship,
+    ref: 'User',
+    index: true,
+  },
+  publishedDate: {
+    type: Types.Date,
+    index: true,
+    dependsOn: {
+      state: 'published',
+    },
+  },
+  image: {
+    type: Types.CloudinaryImage,
+  },
+  oldImage: {
+    type: Types.Url,
+  },
+  description: {
+    type: Types.Markdown,
+    height: 600,
+  },
+  descriptionEnglish: {
+    type: Types.Markdown,
+    height: 200,
+    hidden: true,
+  },
+  tags: {
+    type: Types.Relationship,
+    ref: 'Tag',
+    many: true,
+  },
 });
 
-Entry.schema.virtual('content.full').get(function () {
-    return this.content.extended.html || this.content.brief;
-});
+/* nice defaults */
+
+Entry.schema.virtual('content.full').get(() => this.content.extended.html || this.content.brief);
 
 Entry.defaultColumns = 'title, state|20%, author|20%, publishedDate|20%';
 Entry.register();
--- routes/views/entries.js
+++ routes/views/entries.js
@@ -1,85 +1,75 @@
-var keystone = require('keystone');
-var async = require('async');
+const keystone = require('keystone');
+const async = require('async');
 
-exports = module.exports = function (req, res) {
+module.exports = function (req, res) {
+  const view = new keystone.View(req, res);
+  const locals = res.locals;
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  // Init locals
+  locals.section = 'entries';
+  locals.title = 'Einträge';
+  locals.filters = {
+    tag: req.params.tag,
+  };
+  locals.data = {
+    entries: [],
+    tags: [],
+    activeTag: req.params.tag,
+  };
 
-	// Init locals
-	locals.section = 'entries';
-    locals.title = "Einträge";
-	locals.filters = {
-		tag: req.params.tag,
-	};
-	locals.data = {
-		entries: [],
-		tags: [],
-        activeTag: req.params.tag
-	};
+  // Load all categories
+  view.on('init', (next) => {
+    keystone.list('Tag').model.find().sort('name').exec((err, results) => {
+      if (err || !results.length) {
+        return next(err);
+      }
+      locals.data.tags = results;
 
-	// Load all categories
-	view.on('init', function (next) {
+      // Load the counts for each category
+      async.each(locals.data.tags, (tag, next) => {
+        keystone.list('Entry').model.count().where('tags').in([tag.id]).exec((err, count) => {
+          tag.entryCount = count; // eslint-disable-line no-param-reassign
+          next(err);
+        });
+      }, next);
+    });
+  });
 
-		keystone.list('Tag').model.find().sort('name').exec(function (err, results) {
+  // Load the current category filter
+  view.on('init', (next) => {
+    if (req.params.tag) {
+      keystone.list('Tag').model.findOne({ slug: locals.filters.tag }).exec((err, result) => {
+        locals.data.tag = result;
+        next(err);
+      });
+    } else {
+      next();
+    }
+  });
 
-			if (err || !results.length) {
-				return next(err);
-			}
+  // Load the posts
+  view.on('init', (next) => {
+    const q = keystone.list('Entry').paginate({
+      page: req.query.page || 1,
+      perPage: 100,
+      maxPages: 10,
+      filters: {
+        state: 'published',
+      },
+    })
+  .sort('-publishedDate')
+  .populate('author tags');
 
-			locals.data.tags = results;
+    if (locals.data.tag) {
+      q.where('tags').in([locals.data.tag]);
+    }
 
-			// Load the counts for each category
-			async.each(locals.data.tags, function (tag, next) {
+    q.exec((err, results) => {
+      locals.data.entries = results;
+      next(err);
+    });
+  });
 
-				keystone.list('Entry').model.count().where('tags').in([tag.id]).exec(function (err, count) {
-					tag.entryCount = count;
-					next(err);
-				});
-
-			}, function (err) {
-				next(err);
-			});
-		});
-	});
-
-	// Load the current category filter
-	view.on('init', function (next) {
-
-		if (req.params.tag) {
-			keystone.list('Tag').model.findOne({ slug: locals.filters.tag }).exec(function (err, result) {
-				locals.data.tag = result;
-				next(err);
-			});
-		} else {
-			next();
-		}
-	});
-
-	// Load the posts
-	view.on('init', function (next) {
-
-		var q = keystone.list('Entry').paginate({
-			page: req.query.page || 1,
-			perPage: 10,
-			maxPages: 10,
-			filters: {
-				state: 'published',
-			},
-		})
-			.sort('-publishedDate')
-			.populate('author tags');
-
-		if (locals.data.tag) {
-			q.where('tags').in([locals.data.tag]);
-		}
-
-		q.exec(function (err, results) {
-			locals.data.entries = results;
-			next(err);
-		});
-	});
-
-	// Render the view
-	view.render('entries');
+  // Render the view
+  view.render('entries');
 };
