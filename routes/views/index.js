2225df0db689ba7f20f2420caa9ddd9b32eefe67
Tim von Oldenburg <tim.vonoldenburg@skyscanner.net>
2017-05-07 14:47:18 +0100

    Add newsletter signup on homepage

--- routes/views/index.js
+++ routes/views/index.js
@@ -1,57 +1,52 @@
-var keystone = require('keystone');
+const keystone = require('keystone');
 
 exports = module.exports = function (req, res) {
+  const view = new keystone.View(req, res);
+  const locals = res.locals;
 
-	var view = new keystone.View(req, res);
-	var locals = res.locals;
+  locals.section = 'home';
 
-	// locals.section is used to set the currently selected
-	// item in the header navigation.
-	locals.section = 'home';
+  locals.data = {
+    posts: [],
+    entries: [],
+    categories: [],
+  };
 
-    locals.data = {
-		posts: [],
-        entries: [],
-		categories: [],
-	};
-
-	// Load all categories
-	view.on('init', function (next) {
-
-        var q = keystone.list('Post').model.find()
+  // Load all categories
+  view.on('init', (next) => {
+    const q = keystone.list('Post').model.find()
             .where('state', 'published')
             .populate('category')
             .sort('-publishedDate')
             .limit(5)
             ;
 
-        q.exec(function (err, posts) {
-            locals.data.posts = posts;
-            next(err);
-        });
+    q.exec((err, posts) => {
+      locals.data.posts = posts;
+      next(err);
     });
+  });
 
-    view.on('init', function (next) {
-
-        var q = keystone.list('Entry').model.find()
+  view.on('init', (next) => {
+    const q = keystone.list('Entry').model.find()
             .where('state', 'published')
             // .populate('tags')
             .populate({
-                path: 'tags',
+              path: 'tags',
                 // TODO: only show the 3 most popular ones?
                 // match: { age: { $gte: 21 }},
-                options: { limit: 3 }
+              options: { limit: 3 },
             })
             .sort('-publishedDate')
             .limit(10)
             ;
 
-        q.exec(function (err, entries) {
-            locals.data.entries = entries;
-            next(err);
-        });
+    q.exec((err, entries) => {
+      locals.data.entries = entries;
+      next(err);
     });
+  });
 
-	// Render the view
-	view.render('index');
+  // Render the view
+  view.render('index');
 };
--- routes/views/maps.js
+++ routes/views/maps.js
@@ -1,5 +1,4 @@
 const keystone = require('keystone');
-// const helpers = require('../../templates/views/helpers')();
 
 module.exports = (req, res) => {
   const view = new keystone.View(req, res);
@@ -7,29 +6,6 @@ module.exports = (req, res) => {
 
   // Set locals
   locals.section = 'maps';
-  // locals.filters = {
-  //   page: req.params.page,
-  // };
-  locals.data = {};
-
-  // Load the current post
-  // view.on('init', function (next) {
-  //   var q = keystone.list('Page').model.findOne({
-  //   slug: locals.filters.page,
-  //   });
-    //
-  //   q.exec(function (err, result) {
-  //   locals.data.page = result;
-    //         locals.meta = {
-    //             // title: result.title,
-    //             // description: result.content.brief,
-    //             // url: helpers.fullPostUrl(locals.filters.post),
-    //             // image: ""
-    //         }
-  //   next(err);
-  //   });
-    //
-  // });
 
   // Render the view
   view.render('maps');
